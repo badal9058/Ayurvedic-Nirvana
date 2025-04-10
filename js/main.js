0994268f12554aea8a369528c20fd8e71a368f84
Badal Arya <84496931+badal9058@users.noreply.github.com>
2025-04-10 23:12:22 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -208,14 +208,14 @@
   });
 
   // Init AOS
-  function aos_init() {
+  function AOS_init() {
     AOS.init({
       duration: 1000,
       once: true
     });
   }
   $(window).on('load', function() {
-    aos_init();
+    AOS_init();
   });
 
 })(jQuery);
